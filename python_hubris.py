8906b0e38b6bc53252fba11b0b231ae939c91c83
gommis <47540262+gommis@users.noreply.github.com>
2019-03-11 11:52:31 +1300

    Add files via upload

---
+++ python_hubris.py
@@ -0,0 +1,27 @@
+from bottle import run, route, view, get, post, request
+from itertools import count
+
+class Comic:
+
+    _ids = count (0)
+
+    def __init__(self,comic_name, comic_num): 
+        self.id = next(self._ids)
+        self.comic_name = comic_name
+        self.comic_num = comic_num
+
+
+comics = [
+    Comic("Super Dude", "3"),
+    Comic("Water Woman", "9"),
+    Comic("Lizard man", "13")
+    ]
+
+@route('/')
+@view ('index')
+def index():
+    pass
+
+
+
+run(host='0.0.0.0', port = 8080, reloader=True, debug=True)
